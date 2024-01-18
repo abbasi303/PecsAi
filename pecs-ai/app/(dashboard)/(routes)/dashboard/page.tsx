9ce08fb59cf1dc378ee72b410af735f5a885ac9b
abbasi303 <usalcgg18@gmail.com>
2024-01-18 20:19:27 +0100

    General Frame auth install

---
+++ pecs-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const DashboardPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to the dashboard page!</p>
+        </div>
+    );
+};
+
+export default DashboardPage;
---
+++ pecs-ai/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+
+const LandingPage= () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to the Landing page!</p>
+        </div>
+    );
+};
+
+export default LandingPage;
